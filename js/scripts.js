50d2f1ed899af3bb555413c35429f3a1b3e5e3b8
jude juma <judejuma@gmail.com>
2018-02-23 12:55:24 +0300

    intitial commit

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+});
+$(document).ready(function() {
+        $("p").click(function() {
+          $("img").show();
+        });
+      });
